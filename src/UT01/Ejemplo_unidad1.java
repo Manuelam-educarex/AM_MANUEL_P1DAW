0f350274a535ebdb2b955db1bb7c168d6331a46c
diurno <diurno@equipo-00>
2025-09-23 10:16:33 +0200

    ejemplo clase

---
+++ src/UT01/Ejemplo_unidad1.java
@@ -0,0 +1,22 @@
+package UT01;
+import java.util.Scanner;
+public class Ejemplo_unidad1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce tu nombre");
+		String nombre = sc.nextLine();
+		
+		
+		System.out.println("Introduce tu edad");
+		int edad = sc.nextInt();
+		
+		System.out.println("Hola  "+ nombre+", tienes "+ edad+" años");
+	
+	
+		 
+		
+	}
+
+}
