fee26c582251a48a1b3cf4c0fd1aeba48413b2bc
diurno <diurno@equipo-00>
2025-10-14 11:10:56 +0200

    ejemplo clase

---
+++ src/UT02/Ejemplo1_unidad2.java
@@ -0,0 +1,15 @@
+package UT02;
+
+public class Ejemplo1_unidad2 {
+
+	public static void main(String[] args) {
+		
+		
+		String hola = "hola";
+		System.out.println(hola.length());
+		System.out.println(hola.toUpperCase());
+		System.out.println(hola.contains(hola));
+		
+	}
+
+}
